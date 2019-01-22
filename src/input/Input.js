e46c41decc533d022b031e8263460a3d6cd5821f
Hamzakhann <hamzakhann66@gmail.com>
2019-01-22 21:14:30 +0500

    added inputfield

--- src/input/Input.js
+++ src/input/Input.js
@@ -2,11 +2,44 @@ import React , {Component} from 'react';
 import {StyleSheet, Text, View , TouchableWithoutFeedback, TextInput }  from 'react-native';
 
 class Input extends Component{
+    state = {
+        myInput :""
+    }
+
+    onChangeInput = (value)=>{
+        this.setState({
+            myInput:value
+        })
+    }
     render(){
         return(
-            <TextInput/>
+            <View style={styles.inputWrapper} >
+                <TextInput
+            onChangeText = {this.onChangeInput}
+            style = {styles.input}
+            value={this.state.myInput}
+            multiline={true}
+            maxLength={100}
+            editable={true}
+            autoCapitalize={"words"}
+            />
+            </View>
+            
         )
     }
 }
+
+const styles = StyleSheet.create({
+    inputWrapper:{
+        width:'100%'
+    },
+    input : {
+        width:'100%',
+        backgroundColor:'#f2f2f2',
+        marginTop:20,
+        fontSize:20,
+        padding: 10
+    }
+})
 
 export default Input;
